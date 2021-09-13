973da090557edd4372b0b4f1f149684e3d07fd52
Kyle Awayan <kyle@awayan.com>
2021-09-12 19:20:41 -0700

    add temp reviews page

---
+++ pages/reviews.js
@@ -0,0 +1,12 @@
+import { Container } from "@chakra-ui/layout";
+import React from "react";
+import Menu from "../components/Menu";
+
+export default function reviews() {
+  return (
+    <div>
+      <Menu />
+      <Container mt={8}>Coming soon!</Container>
+    </div>
+  );
+}
