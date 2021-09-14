27bab33babf8684d3564a0edee11b4e8a2f82377
Kyle Awayan <kyle@awayan.com>
2021-09-13 19:36:43 -0700

    get info from db

--- components/Card.js
+++ components/Card.js
@@ -1,145 +1,31 @@
 import React from "react";
 import Image from "next/image";
-import {Container, Heading, Text } from "@chakra-ui/layout";
+import { Container, Heading, Text } from "@chakra-ui/layout";
 import { Box } from "@chakra-ui/react";
 import { Button } from "@chakra-ui/button";
 
-export default function Card({ src, alt, foodName, tasteRating, looksRating, comment}) {
+export default function Card({
+  src,
+  alt,
+  foodName,
+  tasteRating,
+  looksRating,
+  comment,
+}) {
   return (
-    <Box maxW="100%"className="row">
-        <Box w="33%" p={4} className="column">
-            <Image src={src} alt={alt} className="imgCard"/>
-            <Box>
-                <Container mt={8}>
-                    <Text className="headingCard">
-                        Food : {foodName}
-                    </Text>
-                    <Text>
-                        Taste: {tasteRating}/5
-                    </Text>
-                    <Text>
-                        Look: {looksRating}/5
-                    </Text>
-                    <Text>
-                        Comments: {comment}
-                    </Text>
-                </Container>
-                <Button colorScheme="blue" as="a" mb={10}>
-                    Click Me
-                </Button>
-            </Box>
-        </Box>
-        <Box w="33%" p={4} className="column">
-            <Image src={src} alt={alt} className="imgCard"/>
-            <Box>
-                <Container mt={8}>
-                    <Text className="headingCard">
-                        Food : Love
-                    </Text>
-                    <Text>
-                        Taste: 3/5
-                    </Text>
-                    <Text>
-                        Look: 3/5
-                    </Text>
-                    <Text>
-                        Comments: XDXDXD
-                    </Text>
-                </Container>
-                <Button colorScheme="blue" as="a" mb={10}>
-                    Click Me
-                </Button>
-            </Box>
-        </Box>
-        <Box w="33%" p={4} className="column">
-            <Image src={src} alt={alt} className="imgCard"/>
-            <Box>
-                <Container mt={8}>
-                    <Text className="headingCard">
-                        Food : Love
-                    </Text>
-                    <Text>
-                        Taste: 3/5
-                    </Text>
-                    <Text>
-                        Look: 3/5
-                    </Text>
-                    <Text>
-                        Comments: XDXDXD
-                    </Text>
-                </Container>
-                <Button colorScheme="blue" as="a" mb={10}>
-                    Click Me
-                </Button>
-            </Box>
-        </Box>
-        <Box w="33%" p={4} className="column">
-            <Image src={src} alt={alt} className="imgCard"/>
-            <Box>
-                <Container mt={8}>
-                    <Text className="headingCard">
-                        Food : {foodName}
-                    </Text>
-                    <Text>
-                        Taste: 3/5
-                    </Text>
-                    <Text>
-                        Look: 3/5
-                    </Text>
-                    <Text>
-                        Comments: XDXDXD
-                    </Text>
-                </Container>
-                <Button colorScheme="blue" as="a" mb={10}>
-                    Click Me
-                </Button>
-            </Box>
-        </Box>
-        <Box w="33%" p={4} className="column">
-            <Image src={src} alt={alt} className="imgCard"/>
-            <Box>
-                <Container mt={8}>
-                    <Text className="headingCard">
-                        Food : Love
-                    </Text>
-                    <Text>
-                        Taste: 3/5
-                    </Text>
-                    <Text>
-                        Look: 3/5
-                    </Text>
-                    <Text>
-                        Comments: XDXDXD
-                    </Text>
-                </Container>
-                <Button colorScheme="blue" as="a" mb={10}>
-                    Click Me
-                </Button>
-            </Box>
-        </Box>
-        <Box w="33%" p={4} className="column">
-            <Image src={src} alt={alt} className="imgCard"/>
-            <Box>
-                <Container mt={8}>
-                    <Text className="headingCard">
-                        Food : Love
-                    </Text>
-                    <Text>
-                        Taste: 3/5
-                    </Text>
-                    <Text>
-                        Look: 3/5
-                    </Text>
-                    <Text>
-                        Comments: XDXDXD
-                    </Text>
-                </Container>
-                <Button colorScheme="blue" as="a" mb={10}>
-                    Click Me
-                </Button>
-            </Box>
-        </Box>
-        
+    <Box w="33%" p={4} className="column">
+      <Image src={src} alt={alt} className="imgCard" />
+      <Box>
+        <Container mt={8}>
+          <Text className="headingCard">Food : {foodName}</Text>
+          <Text>Taste: {tasteRating}/5</Text>
+          <Text>Look: {looksRating}/5</Text>
+          <Text>Comments: {comment}</Text>
+        </Container>
+        <Button colorScheme="blue" as="a" mb={10}>
+          Click Me
+        </Button>
+      </Box>
     </Box>
   );
 }
--- pages/reviews.js
+++ pages/reviews.js
@@ -1,18 +1,38 @@
-import { Container, Box} from "@chakra-ui/layout";
+import { Container, Box, Flex } from "@chakra-ui/layout";
 import React from "react";
 import Menu from "../components/Menu";
 import Image from "next/image";
 import pavPic from "../public/pav.jpg";
 import Card from "../components/Card";
 import { supabase } from "../lib/supabaseClient";
-export default function reviews() {
-  // let { data: Survey, error } = await supabase
-  // .from('Survey')
-  // .select("id")
+
+export default function reviews({ Food }) {
   return (
     <div>
       <Menu />
-        <Card src={pavPic} alt="The Pavillion" foodName="Burgers" tasteRating="1" looksRating="2" comment="Booty"/>
+      <Box maxW="100%" className="row">
+        {Food.map((foodItem) => (
+          <Card
+            src={pavPic}
+            alt="The Pavillion"
+            foodName={foodItem.FoodTitle}
+            tasteRating="1"
+            looksRating="2"
+            comment="Booty"
+            key={foodItem.id}
+          />
+        ))}
+      </Box>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  let { data: Food, error } = await supabase
+    .from("Food")
+    .select("id, FoodTitle");
+
+  return {
+    props: { Food },
+  };
 }
