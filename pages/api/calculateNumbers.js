9bf4ae0cab3c853af45f04c51f8db0e3538d41de
Kyle Awayan <kyle@awayan.com>
2021-09-21 20:31:48 -0700

    git mv didn't really work

--- pages/api/calculateNumbers.js
+++ pages/api/calculateNumbers.js
@@ -1,15 +1,51 @@
-const { Pool, Client } = require("pg");
+const { Client } = require("pg");
 
 export default async function handler(req, res) {
-  const client = new Client();
-  await client.connect();
+  if (req.method === "POST") {
+    const reqFoodId = req.body.reqFoodId;
 
-  const response = await client.query(
-    `SELECT COUNT("FoodId") FROM "Survey" WHERE "FoodId" = 1`
-  );
+    if (!reqFoodId) {
+      res.status(400).json({ error: "Invalid request" });
+      return;
+    }
 
-  console.log(response);
-  await client.end();
+    const client = new Client();
+    await client.connect();
 
-  res.status(200).json({ name: "John Doe" });
+    await client.query(
+      `UPDATE "Food"
+      SET "AverageLooks" = (
+        SELECT AVG("Rating_Looks") FROM "Survey"
+        WHERE "FoodId" = $1
+      )
+      WHERE id = $1`,
+      [reqFoodId]
+    );
+
+    await client.query(
+      `UPDATE "Food"
+      SET "AverageTaste" = (
+        SELECT AVG("Rating_Taste") FROM "Survey"
+        WHERE "FoodId" = $1
+      )
+      WHERE id = $1`,
+      [reqFoodId]
+    );
+
+    await client.query(
+      `UPDATE "Food"
+      SET "ReviewCount" = (
+        SELECT COUNT("FoodId") FROM "Survey"
+        WHERE "FoodId" = $1
+      )
+      WHERE id = $1`,
+      [reqFoodId]
+    );
+
+    await client.end();
+
+    res.status(200).json({ status: "Success" });
+  } else {
+    res.status(400).json({ error: "Invalid request" });
+  }
 }
