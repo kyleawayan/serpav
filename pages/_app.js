19dd0ad2b77f6ef1cf9d2e33d126b946eac8df68
Kyle Awayan <kyle@awayan.com>
2021-09-24 20:58:46 -0700

    don't index or follow pages

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import theme from "../lib/theme";
 import NProgress from "nprogress";
 import "../styles/nprogress.css";
 import Router from "next/router";
+import { DefaultSeo } from "next-seo";
 
 import "@fontsource/inter/400.css";
 import "@fontsource/inter/700.css";
@@ -14,6 +15,10 @@ Router.events.on("routeChangeError", () => NProgress.done());
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
+      <DefaultSeo
+        dangerouslySetAllPagesToNoIndex={true}
+        dangerouslySetAllPagesToNoFollow={true}
+      />
       <Component {...pageProps} />
     </ChakraProvider>
   );
